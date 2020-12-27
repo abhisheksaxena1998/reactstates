05ebd90a7e180ee8e4e1539bd98d2c43759d9e74
abhisheksaxena1998 <abhi123923@gmail.com>
2020-12-27 12:28:07 +0530

    state

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+### Live at : https://abhisheksaxena1998.github.io/reactstates/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,22 +3,27 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = 
     {   
-        count:1      
+        count:0      
     };
     styles={
         fontSize:14,
         fontWeight:"bold"
     };
     render() { 
-        let classes="badge m-2 badge-"
-        classes+= (this.state.count===0)?"warning":"info"
+        let classes = this.getBadgeClasses();
         return (  
         <React.Fragment>
             <span style={ this.styles } className="badge badge-primary m-4">{this.formatCount()}</span>
-            <button className={ classes }>Increment</button>
+            <button className={ this.getBadgeClasses() }>Increment</button>
         </React.Fragment>
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "info";
+        return classes;
+    }
+
     formatCount()
     {
         const {count}=this.state;
